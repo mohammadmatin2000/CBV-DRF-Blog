e16ec630535ed65c1e5193b4236bb692620bdef8
MATIN <mohammadmatin13872008@gmail.com>
2025-04-27 16:41:54 +0330

    Creating base views and forms

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path
-from .views import IndexView,RedirectToMakt
-
+from .views import IndexView, RedirectToMakt, PostDetailView, PostListView, PostFormView
 
 # ======================================================================================================================
+# Setting the application namespace to 'blog' for URL reversibility
 app_name = 'blog'
 
+# Defining URL patterns for the blog application
 urlpatterns = [
-    path('indexclass/', IndexView.as_view(), name='indexclass'),
-    path("go-to-makt/",RedirectToMakt.as_view(), name='go-to-maktabkhooneh'),
+    # Maps '/index-class/' to the IndexView class-based view
+    path('index-class/', IndexView.as_view(), name='index-class'),
+
+    # Maps '/go-to-makt/' to the RedirectToMakt view, handling redirections
+    path("go-to-makt/", RedirectToMakt.as_view(), name='go-to-maktabkhooneh'),
+
+    # Maps '/post-list/' to the PostListView class-based view, displaying all posts
+    path('post-list/', PostListView.as_view(), name='post-list'),
+
+    # Maps '/post-list/<int:pk>' to the PostDetailView class-based view, showing a specific post by primary key (pk)
+    path('post-list/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+
+    # Maps '/post-create/' to the PostFormView class-based view, handling post creation
+    path('post-create/', PostFormView.as_view(), name='post-create'),
 ]
+
+# ======================================================================================================================
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,17 +1,73 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView,RedirectView
+from django.views.generic import TemplateView, RedirectView, DetailView, FormView, CreateView
+from django.views.generic import ListView
 from .models import Post
-# ======================================================================================================================
+from .forms import PostForm
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Blog'
-        context['posts'] = Post.objects.all()
-        return context
 # ======================================================================================================================
+# IndexView: A template-based view that renders the 'index.html' page
+class IndexView(TemplateView):
+    template_name = 'index.html'  # Specifies which template should be used
+
+    def get_context_data(self, **kwargs):
+        """
+        This method adds extra context data to the template.
+        """
+        context = super().get_context_data(**kwargs)  # Gets the default context
+        context['title'] = 'Blog'  # Adds a title to the context
+        context['posts'] = Post.objects.all()  # Fetches all blog posts
+        return context
+
+# ======================================================================================================================
+# RedirectToMakt: A redirect view that directs users to an external website
 class RedirectToMakt(RedirectView):
-    url = 'https://maktabkhooneh.com/'
+    url = 'https://maktabkhooneh.com/'  # Redirects users to this URL
+
+# ======================================================================================================================
+# PostListView: A class-based view to display a paginated list of posts
+class PostListView(ListView):
+    model = Post  # Specifies the model to retrieve objects from
+    context_object_name = 'posts'  # Assigns a name to the list of posts in the template
+    paginate_by = 2  # Limits the number of posts displayed per page
+    ordering = 'id'  # Orders posts by their ID (ascending order)
+
+    # Uncommented version could be used to filter only published posts
+    # queryset = Post.objects.all()
+    # def get_queryset(self):
+    #     return Post.objects.filter(status=1)
+
+# ======================================================================================================================
+# PostDetailView: A class-based view to display the details of a single post
+class PostDetailView(DetailView):
+    model = Post  # Specifies the model to retrieve a single post instance
+
+# ======================================================================================================================
+# PostFormView: A class-based view for handling post creation using Django's CreateView
+class PostFormView(CreateView):
+    model = Post  # Specifies the model the form interacts with
+    form_class = PostForm  # Defines the form class used to create a post
+    success_url = '/postlist/'  # Redirects to the post list page after successful form submission
+
+    def form_valid(self, form):
+        """
+        Overwrites the form_valid method to assign the logged-in user as the author.
+        """
+        form.instance.author = self.request.user  # Sets the author to the currently logged-in user
+        return super().form_valid(form)  # Calls the parent method to complete form submission
+
+    # Alternative approach: Fields could be explicitly listed instead of using a form class
+    # fields = ['title', 'content','category','status']
+
+# ======================================================================================================================
+"""
+Alternative PostFormView using FormView instead of CreateView:
+class PostFormView(FormView):
+    template_name = "contact.html"  # Specifies the template used for displaying the form
+    form_class = PostForm  # Defines the form class for post submission
+    success_url = "/postlist/"  # Redirects to post list on successful form submission
+
+    def form_valid(self, form):
+        form.save()  # Saves the form instance
+        return super().form_valid(form)
+"""
 # ======================================================================================================================
