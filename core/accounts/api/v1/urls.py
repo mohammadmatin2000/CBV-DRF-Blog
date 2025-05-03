4925477d2e22513e9f057b13b806e87fd78ec67a
MATIN <mohammadmatin13872008@gmail.com>
2025-05-03 23:00:17 +0330

    Creating Registration and Token Authentication

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegistrationApiView
+from .views import RegistrationApiView,CustomAuthToken,CustomDeleteToken
+
 # ======================================================================================================================
 app_name = 'api-v1'
 urlpatterns = [
     # registeration
     path('registeration/',RegistrationApiView.as_view(),name='register'),
+    path('token/login/',CustomAuthToken.as_view(),name='token-login'),
+    path('token/logout/',CustomDeleteToken.as_view(),name='token-logout'),
     # change password
     # reset password
     # login token
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer,CustomAuthTokenSerializer
 from ...models import User
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 # ======================================================================================================================
 class RegistrationApiView(generics.GenericAPIView):
@@ -17,4 +21,24 @@ class RegistrationApiView(generics.GenericAPIView):
             }
             return Response(data, status=status.HTTP_201_CREATED)
 
+# ======================================================================================================================
+class CustomAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+# ======================================================================================================================
+class CustomDeleteToken(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+       request.user.auth_token.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
 # ======================================================================================================================
