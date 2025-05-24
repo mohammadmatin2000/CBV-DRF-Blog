901ad65c4552b9d82a60372fd8dce609a1b0c804
MATIN <mohammadmatin13872008@gmail.com>
2025-05-24 20:02:44 +0330

    creating cache
    creating cache

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from .views import send_email,test
+from .views import weather_view
+
 # ======================================================================================================================
 app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("accounts.api.v1.urls")),
     path('send_email/', send_email, name='sendemail'),
-    path("test/",test,name="test")
+    path("test/",test,name="test"),
+    path('api/weather/', weather_view, name='weather'),
     # path('api/v2/', include('djoser.urls')),
     # path('api/v2/', include('djoser.urls.jwt')),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.cache import cache_page
 from .tasks import sendemail
 import requests
+
 
 # ======================================================================================================================
 def send_email(request):
@@ -13,4 +14,11 @@ def send_email(request):
 def test(request):
     response = requests.get("https://02505983-6e95-4435-a523-127e06cc0410.mock.pstmn.io/test/delay/5")
     return HttpResponse(response)
+
+# ======================================================================================================================
+@cache_page(20 * 60)
+def weather_view(request):
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=35.6892&longitude=51.3890&current_weather=true")
+    return JsonResponse(response.json())
 # ======================================================================================================================
